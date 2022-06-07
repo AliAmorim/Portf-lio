395a55a0ce327cf443bc69d7e8922ed7e33479e8
AliAmorim <alineamorimxd@hotmail.com>
2022-06-07 20:04:32 -0300

    commit

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
-let botao = document.getElementById('btnenviar')
-
-botao.onclick = function enviar () {
-
-    alert("Enviado com sucesso!");
-    window.location.href = "index.html";
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+    
+  });
 }
+const titulo = document.querySelector('.descricao');
+typeWrite(titulo);
