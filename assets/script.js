f06e840b2c1b98d3d0efeb65ef8bfa92c80ac2c3
AliAmorim <alineamorimxd@hotmail.com>
2022-06-07 21:03:59 -0300

    att

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,13 @@
 function typeWrite(elemento){
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = ' ';
-    textoArray.forEach(function(letra, i){   
+    textoArray.forEach(function(letra, i, ){   
       
     setTimeout(function(){
         elemento.innerHTML += letra;
     }, 75 * i)
     
+
   });
 }
 const titulo = document.querySelector('.descricao');
